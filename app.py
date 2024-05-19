1cf5ea06d960a0301f5e675d123d0b870f418382
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2024-05-19 16:24:46 +0530

    Web finalized

--- app.py
+++ app.py
@@ -18,9 +18,6 @@ config = {
 firebase_url = 'https://pok-gdwd-kdfnihvw-default-rtdb.firebaseio.com/'
 firebase_app = firebase.FirebaseApplication(firebase_url, None)
 
-result = firebase_app.get('/users', None)
-print(result)
-
 model = pickle.load(open('model_heart_disease.pkl', 'rb')) 
 
 app = Flask(__name__)
@@ -37,38 +34,57 @@ def register():
 def login():
     return render_template('login.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('db.html')
-
 @app.route('/signup', methods =['POST'])
 def signup():
     data = dict(request.form)
-    x = random.randint(1,1000)
     email = data['email']
     password = data['password']
 
-    raw = {'id': x, 'email': email, 'password': password}
+    raw = {'email': email, 'password': password}
 
     response = firebase_app.post('/users', raw)
     print(response)
 
     return render_template('register.html')
 
+@app.route('/signin', methods=['POST'])
+def signin():
+    data = dict(request.form)
+    email = data['username']
+    password = data['password']
+
+    result = firebase_app.get('/users', None)
+    print(result)
+
+    user_found = False
+    for key, value in result.items():
+        if value['email'] == email:
+            user_found = True
+            if value['password'] == password:
+                return render_template('db.html')
+            else:
+                error_message = "Wrong Password"
+                return render_template('login.html', error=error_message)
+    if not user_found:
+        error_message = "Invalid User"
+        return render_template('login.html', error=error_message)
+
+    return render_template('login.html')
+
 @app.route('/predict', methods =['POST'])
 def predict():
     features = [float(i) for i in request.form.values()]
     array_features = [np.array(features)]
+    data = [int(i) for i in request.form.values()]
+    print(data)
 
     prediction = model.predict(array_features)
     output = prediction
     
     if output == 1:
-        return render_template('db.html', 
-                               result = 'Heart disease: Unlikely')
+        return render_template('db.html', result = 'Heart disease: Unlikely', data = data, output = output)
     else:
-        return render_template('db.html', 
-                               result = 'Heart disease: Likely')
+        return render_template('db.html', result = 'Heart disease: Likely', data = data, output = output)
 
 if __name__ == '__main__':
     app.run()
